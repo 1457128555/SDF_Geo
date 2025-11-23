52c4b58643ece1177ce48f47d03e5a6d4dabbb77
chensheng <1457128555@qq.com>
2025-11-23 20:47:11 +0800

    fix: correct CMakeLists.txt paths and update documentation for new structure

--- build.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-REM Wrapper script - calls the actual build script in scripts/
-call scripts\build.bat %*
--- run.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-REM Wrapper script - calls the actual run script in scripts/
-call scripts\run.bat %*
